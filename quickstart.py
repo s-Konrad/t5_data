06805f99e9accf31b079f9b5eb75a36267692859
Konrad <skwierczynskikonrad@gmail.com>
2025-12-10 11:55:19 +0100

    move data to df and add column for total output

--- quickstart.py
+++ quickstart.py
@@ -1,13 +1,26 @@
+from typing import *
 from config import API_KEY, SPREADSHEET_ID, RANGE_NAME, SCOPES
 from googleapiclient.discovery import build
+import pandas as pd
+import streamlit as st
 
 
+def values_from_spreadsheet() -> List[Any]:
+    service = build("sheets", "v4", developerKey=API_KEY)
+    sheet = service.spreadsheets()
+    sheet_read = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+    values = sheet_read.get("values", [])
+    return values
 
 
-service = build("sheets", "v4", developerKey=API_KEY)
-sheet = service.spreadsheets()
+def generate_total_column(values: List[Any]):
+    df = pd.DataFrame(values[1:], columns=values[0])
+    df['Total'] = 0
+    for col in df.columns[1:-1]:
+        df[col] = pd.to_numeric(df[col], errors='coerce')
+        df['Total'] += df[col]
+    return df
 
-
-sheet_read = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
-
-values = sheet_read.get("values", [])
+def main():
+    values = values_from_spreadsheet()
+    mines_df = generate_total_column(values)
